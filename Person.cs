518a002e4ed425330622b09c2262a593b87c12ed
Gülşen <gulsenk48@hotmail.com>
2024-10-16 21:02:05 +0300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstClassApp
+{
+    public class Person
+    {
+        public string name;
+        public string lastName;
+        public string birthDate;
+
+        public string Name { get{ return name; } set { name = value; } }
+
+        public string LastName { get { return lastName;} set {lastName = value;} }
+
+        public string BirthDate { get { return birthDate;} set {birthDate = value;} }
+
+        public void info()
+        {
+            Console.WriteLine("\nName: " + name + "\nLastname: " + lastName + "\nBirthdate: " + birthDate);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using firstClassApp;
+
+Person firstStudent = new Person();
+firstStudent.Name = "Emily";
+firstStudent.lastName = "Cooper";
+firstStudent.birthDate = "16.10.1990";
+
+firstStudent.info();
+
+Person secondStudent = new Person();
+secondStudent.Name = "Mindy";
+secondStudent.lastName = "Chen";
+secondStudent.birthDate = "20.08.1988";
+
+secondStudent.info();
+
+Person firstTeacher = new Person();
+firstTeacher.Name = "Pierre";
+firstTeacher.lastName = "Cadault";
+firstTeacher.birthDate = "02.07.1960";
+
+firstTeacher.info();
+
+Person secondTeacher = new Person();
+secondTeacher.Name = "Antoine";
+secondTeacher.lastName = "Lambert";
+secondTeacher.birthDate = "16.12.1975";
+
+firstTeacher.info();
